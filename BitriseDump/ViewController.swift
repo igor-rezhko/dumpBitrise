1676197f7ed3174c43dfa693978da3791a5f684e
Igor Rezhko <igor_rezhko@epam.com>
2023-02-02 09:37:25 +0200

    initial commit

---
+++ BitriseDump/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitriseDump
+//
+//  Created by Igor Rezhko on 02.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
